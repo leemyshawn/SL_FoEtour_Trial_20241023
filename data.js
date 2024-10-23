ea8caff0e163f0936c05e17889095e66e85ccd50
leemyshawn <leemyshawn@gmail.com>
2024-10-23 15:43:24 +0800

    First Commit
    First commit (ie change) of my tour

---
+++ data.js
@@ -0,0 +1,176 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-along-engineering-drive-1",
+      "name": "Along Engineering Drive 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 0.19160267960028854,
+        "pitch": 0.023892903189654646,
+        "fov": 1.4758882578279586
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7287403225832456,
+          "pitch": -0.2881036490960778,
+          "rotation": 7.0685834705770345,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.6483337648948613,
+          "pitch": 0.0360757437058421,
+          "title": "Faculty of Engineering",
+          "text": "Welcome!"
+        },
+        {
+          "yaw": 3.100147541428923,
+          "pitch": -0.04931433154473197,
+          "title": "University Cultural Centre",
+          "text": "Watch out for our performances!"
+        },
+        {
+          "yaw": -1.978744624804719,
+          "pitch": 0.05680340947700202,
+          "title": "Kent Ridge Road",
+          "text": "This way to Yusof Ishak House (UC)!"
+        }
+      ]
+    },
+    {
+      "id": "1-engineering-auditorium",
+      "name": "Engineering Auditorium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -1.1359816914067444,
+        "pitch": -0.18028448698105493,
+        "fov": 1.4758882578279586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9943308419899317,
+          "pitch": -0.1027444254496288,
+          "rotation": 10.210176124166829,
+          "target": "0-along-engineering-drive-1"
+        },
+        {
+          "yaw": 0.019846077660188755,
+          "pitch": -0.012006163390051228,
+          "rotation": 0.7853981633974483,
+          "target": "2-engineering-atrium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.2919632702889778,
+          "pitch": -0.12713089571042602,
+          "title": "Knot Statue",
+          "text": "Just some name i randomly came up with."
+        },
+        {
+          "yaw": 0.20852444522965463,
+          "pitch": -0.5937783604816076,
+          "title": "This way to engineering",
+          "text": "This way for cafe and food!"
+        }
+      ]
+    },
+    {
+      "id": "2-engineering-atrium",
+      "name": "Engineering Atrium",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": -0.5395343905078214,
+        "pitch": -0.2870689670179214,
+        "fov": 1.4758882578279586
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.1140027798557934,
+          "pitch": 0.05489672880088747,
+          "rotation": 5.497787143782138,
+          "target": "1-engineering-auditorium"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3199465038439193,
+          "pitch": -0.20863315544265326,
+          "title": "Ex-Spinelli Coffee",
+          "text": "Now is He by He Brews (?)"
+        },
+        {
+          "yaw": 0.08985995104443845,
+          "pitch": -1.2891676100193958,
+          "title": "Skylight",
+          "text": "Ahhhh my eyessss~"
+        }
+      ]
+    }
+  ],
+  "name": "SL_FoEngineeringtrial",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
